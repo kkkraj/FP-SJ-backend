0e4cbf4042e91e858e1346684bc1e43d1be340f4
github username <you@example.com>
2020-07-19 20:03:00 -0700

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user1 = User.create(name:"Khnaittha Krajangjaem", username:"KhanitthaK15", password:"1115")
+user2 = User.create(name:"Thorin Wilder", username:"tbtbbus", password:"1234")
