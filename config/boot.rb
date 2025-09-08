83fa3cfce0f626455ad18a887a0324e9805ccc06
Khanittha Krajangjaem <kkraj.swe@gmail.com>
2025-09-07 22:25:52 -0700

    Move Logger fix to boot.rb for earlier loading

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+# Fix for Ruby 3.2.0 Logger compatibility
+require 'logger'
+require 'logger/severity'
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
